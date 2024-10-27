03a7a307d6e8035db8d03223f61df724ebdf91ec
FlameMaster64 <hassan.abukar.abubakar@gmail.com>
2024-10-27 13:13:39 -0500

    Update AbubakarHassanIMU.java

--- IMU/AbubakarHassanIMU.java
+++ IMU/AbubakarHassanIMU.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -12,7 +14,7 @@ import static com.qualcomm.hardware.rev.RevHubOrientationOnRobot.xyzOrientation;
 
 @Autonomous
 
-public class AbubakarHassanIMU {
+public class AbubakarHassanIMU extends LinearOpMode{
 
     double kp = 0.5;
     double ki = 0.5;
@@ -29,13 +31,13 @@ public class AbubakarHassanIMU {
     
     IMU imu;
 
-    @Override public void runOpMode() throws InterruptedException {
+    @Override 
+    public void runOpMode() throws InterruptedException {
 
         double xRotation = 0;
         double yrotation = 0;
         double zRotation = 0;
 
-        Orientation hubRotation = xyzOrientation(xRotation, yRotation, zRotation);
         
         frontleftDrive  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontrightDrive = hardwareMap.get(DcMotor.class, "frontRight");
@@ -43,7 +45,7 @@ public class AbubakarHassanIMU {
         backrightDrive  = hardwareMap.get(DcMotor.class, "backRight");
         
         frontleftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontrightDrive.setDirection(DcMotor.Direction.FOWARD);
+        frontrightDrive.setDirection(DcMotor.Direction.FORWARD);
         backleftDrive.setDirection(DcMotor.Direction.REVERSE);
         backrightDrive.setDirection(DcMotor.Direction.FORWARD);
 
@@ -53,8 +55,7 @@ public class AbubakarHassanIMU {
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
         RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.DOWN;
 
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(hubRotation);
-
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
         imu.initialize(new IMU.Parameters(orientationOnRobot));
         
@@ -64,20 +65,13 @@ public class AbubakarHassanIMU {
 
         while (!isStopRequested()) {
             
-            double power = PIDController(angleRadians, imu.getAngularOrientation().firstAngle);
-            frontLeftDrive.setPower(power);
-            backLeftDrive.setPower(power);
-            frontRightDrive.setPower(-power);
-            backRightDrive.setPower(-power);
+//            double power = PIDController(angleRadians, imu.getAngularOrientation().firstAngle);
+//            frontLeftDrive.setPower(power);
+//            backLeftDrive.setPower(power);
+//            frontRightDrive.setPower(-power);
+//            backRightDrive.setPower(-power);
 
             telemetry.addData("Hub orientation", "Logo=%s   USB=%s\n ", logoDirection, usbDirection);
-
-            // if (gamepad1.y) {
-            //     telemetry.addData("Yaw", "Resetting\n");
-            //     imu.resetYaw();
-            // } else {
-            //     telemetry.addData("Yaw", "Press Y (triangle) on Gamepad to reset\n");
-            // }
 
             YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
             AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
@@ -102,14 +96,14 @@ public class AbubakarHassanIMU {
         return proportionError * kp + integral * ki + derivative * kd;
     }
 
-  public double anglewrap(double radians) {
-    while (radians > 180) {
-        radians -= 360;
+  public double anglewrap(double degrees) {
+    while (degrees > 180) {
+        degrees -= 360;
       }
-   while (radians < -180) {
-        radians += 360;
+   while (degrees < -180) {
+        degrees += 360;
       }
-      return radians;
+      return degrees;
   }
 
-}   
+}
