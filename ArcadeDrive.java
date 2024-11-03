27cef8407cf9c8d3cb40d873f017623188824f30
FlameMaster64 <hassan.abukar.abubakar@gmail.com>
2024-11-03 11:38:51 -0600

    Update ArcadeDrive.java

--- ArcadeDrive.java
+++ ArcadeDrive.java
@@ -1,3 +1,32 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -6,59 +35,105 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+ 
 
-@TeleOp(name = "AbubakarHassanArcadeDrive", group = "Linear OpMode")
 
-public class AbubakarHassanArcadeDrive extends LinearOpMode {
 
-    private ElapsedTime runtime = new ElapsedTime();
+
+/*
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "OmarArcadeMode", group = "LinearOpMode")
+
+public class OmarArcadeMode extends LinearOpMode {
     private DcMotor frontleftDrive = null;
-    private DcMotor frontrightDrive = null;
     private DcMotor backleftDrive = null;
+    private DcMotor frontrightDrive = null;
     private DcMotor backrightDrive = null;
+    Servo hookL;
+    Servo hookR;
+    
+
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        
-        frontleftDrive  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontrightDrive = hardwareMap.get(DcMotor.class, "frontRight");
-        backleftDrive  = hardwareMap.get(DcMotor.class, "backLeft");
-        backrightDrive  = hardwareMap.get(DcMotor.class, "backRight");
+        frontleftDrive = hardwareMap.get(DcMotor.class, "fl");
+        frontrightDrive = hardwareMap.get(DcMotor.class, "fr");
+        backleftDrive = hardwareMap.get(DcMotor.class, "bl");
+        backrightDrive = hardwareMap.get(DcMotor.class, "br");
         
         frontleftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontrightDrive.setDirection(DcMotor.Direction.REVERSE);
-        backleftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontrightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backleftDrive.setDirection(DcMotor.Direction.REVERSE);
         backrightDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        double drive, turn, strafe;
+        double flpower, frpower, blpower, brpower;
         waitForStart();
-        runtime.reset();
 
+        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             
-            double leftPower;
-            double rightPower;
+            drive =-gamepad1.left_stick_y;
+            strafe =gamepad1.left_stick_x;
+            turn =gamepad1.right_stick_x;
             
-            double drive = -gamepad1.left_stick_y;
-            double strafe = -gamepad1.left_stick_x;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + strafe + turn, -1.0, 1.0);
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0);
-            frontleftDrive.setPower(leftPower);
-            frontrightDrive.setPower(leftPower);
-            backleftDrive.setPower(rightPower);
-            backrightDrive.setPower(rightPower);
-
-
+            frpower = drive - turn - strafe;
+            brpower = drive - turn + strafe;
+            flpower = drive + turn + strafe;
+            blpower = drive + turn - strafe;
             
+            double maxPower = Math.max(+-       Math.abs(frpower),Math.max(Math.abs(brpower),Math.max(Math.abs(flpower), Math.abs(blpower))));
+            if(maxPower > 1){
+                frpower /= maxPower;
+                brpower /= maxPower;
+                flpower /= maxPower;
+                blpower /= maxPower;
+            }
+            
+            frontrightDrive.setPower(frpower);
+            backrightDrive.setPower(brpower);
+            frontleftDrive.setPower(flpower);
+            backleftDrive.setPower(blpower);
+          
+         if (gamepad1.y){
+              
+              hookL.setPosition(0);
+              
+          } 
+          
+          else if(gamepad1.x){
+              hookL.setPosition(0.7);
+              
+          }
+          
+          else if(gamepad1.a){
+              
+              hookL.setPosition(1);
+              
+          }
 
-
-
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+         
+            
+            
+            
+            telemetry.addData("Status", "Running");
             telemetry.update();
+
         }
     }
 }
-//
