dc9335301377c03342f981ecbe8b4301bf917f86
FlameMaster64 <hassan.abukar.abubakar@gmail.com>
2025-02-27 19:35:30 -0600

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -3,28 +3,39 @@ public class MyClass {
   public static void main(String args[]) {
       
       Scanner input = new Scanner(System.in);
+      
       double num1 = input.nextDouble();
+      
       String operation = input.nextLine();
+      
       double num2 = input.nextDouble();
-   
+      
+      System.out.println(Calc(6, "+", 4));
+  
+   }
+      
+  public static double Calc(double num1, String operation, double num2){
+      
+      double nothing = 0;
+      
       if(operation=="+"){
           double answer = num1 + num2;
-          System.out.println(answer);
+          return answer;
       }
-      else if(operation=="-"){
+      else if(operation.equals("-")){
           double answer = num1 - num2;
-          System.out.println(answer);
+          return answer;
       }
-      else if(operation=="*"){
+      else if(operation.equals("*")){
           double answer = num1 * num2;
-          System.out.println(answer);
+          return answer;
       }
-      else if(operation=="/"){
+      else if(operation.equals("/")){
           double answer = num1 / num2;
-          System.out.println(answer);
+          return answer;
       }
       else{
-          System.out.println("Sorry");
-     }
-   }
+          return nothing;
+      }
+  }
 }
